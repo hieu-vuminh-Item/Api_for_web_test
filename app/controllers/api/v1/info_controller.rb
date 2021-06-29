7a19e704c40f6f7305fd8269aa72d59d8d28b3ba
Hieu.vuminh <hieu.vuminh@pixta.co.jp>
2021-06-29 16:44:49 +0700

    encrypt password

--- app/controllers/api/v1/info_controller.rb
+++ app/controllers/api/v1/info_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::InfoController < ApplicationController
   def user_info
     a = Account.where(:username => params[:user]).first
-    x = a.role_id
-    p a.role_id
+    storable_string = a.password.to_s
+    restored_hash = BCrypt::Password.new storable_string
+    a.password = restored_hash
     if a.role_id == 1
       sql = "select a.username, r.name as 'role_name', s.name as 'status_name'
             from accounts a join roles r on a.role_id = r.id
